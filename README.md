b7c0ea1e6ab847373ae6425328d4edd1837d9533
liyulingyue <852433440@qq.com>
2025-03-20 21:31:12 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CameraUSBHid
+
+## 模型来源
+更多信息请参考
+- https://github.com/openvinotoolkit/open_model_zoo/blob/master/demos/human_pose_estimation_demo/python/README.md
+- https://github.com/openvinotoolkit/openvino_notebooks/blob/latest/notebooks/pose-estimation-webcam/pose-estimation.ipynb
+- https://storage.openvinotoolkit.org/repositories/open_model_zoo/2022.1/models_bin/3/human-pose-estimation-0001/FP16-INT8/
---
+++ main.py
@@ -0,0 +1,24 @@
+import cv2
+
+from Tools.decoder import OpenPoseDecoder
+from Tools.utils import create_model, model_predict, draw_poses
+
+if __name__ == '__main__':
+    decoder = OpenPoseDecoder()
+    model_infor = create_model("Models/human-pose-estimation-0001/FP16-INT8/human-pose-estimation-0001.xml")
+    frame = cv2.imread('example.jpg')
+    poses = model_predict(frame, model_infor, decoder)
+    # print(poses)
+    drawed_frame = draw_poses(frame, poses, point_score_threshold=0.1)
+    # cv2.imshow('image', drawed_frame)
+    # cv2.waitKey(0)
+
+    # 测试速率，进行1000次预测，记录时间，取均值，输出FPS
+    import time
+    start_time = time.time()
+    test_range = 10000
+    for i in range(test_range):
+        poses = model_predict(frame, model_infor, decoder)
+    end_time = time.time()
+    print("fps is:",1 / ((end_time - start_time) / test_range))
+
